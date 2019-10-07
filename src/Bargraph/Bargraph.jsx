4c6a17711b020ffe3e92d279334c9ba7b7ce4b2a
Lynne Rang <lynne.rang@gmail.com>
2019-10-07 14:04:47 -0600

    Add refactor based on minor change to svg where diodes are within their own group.

--- src/Bargraph/Bargraph.jsx
+++ src/Bargraph/Bargraph.jsx
@@ -3,33 +3,21 @@ import { SvgLoader, SvgProxy } from 'react-svgmt';
 import Image from '../images/o9-Bargraph-01.svg';
 import PropTypes from 'prop-types';
 
-const Bargraph = (props) => {
-  const getClass = () => {
-    const { power } = props;
-    let result = `power-${Math.floor(power)}`;
-    
-    return power % 1 === 0 ? result : `${result} next-partial`;
-  }
+const Bargraph = props => {
+	const { volts } = props;
+	const voltsWhole = Math.floor(volts);
 
-  console.log(Math.floor(props.power))
-
-  return (
-    <SvgLoader className={getClass()} path={Image}>
-      <SvgProxy
-        selector={`.diodes .st0:nth-child(-n+${Math.floor(props.power)})`}
-        class="st0 active"
-      />
-      {/* <SvgProxy selector="rect:nth-of-type(4)" class="st0 diode-5" />
-      <SvgProxy selector="rect:nth-of-type(6)" class="st0 diode-4" />
-      <SvgProxy selector="rect:nth-of-type(8)" class="st0 diode-3" />
-      <SvgProxy selector="rect:nth-of-type(10)" class="st0 diode-2" />
-      <SvgProxy selector="rect:nth-of-type(12)" class="st0 diode-1" /> */}
-    </SvgLoader>
-  );
+	return (
+		<SvgLoader path={Image}>
+			<SvgProxy selector={`.diodes .st0:nth-child(-n+${voltsWhole})`} class="st0 active" />
+			<SvgProxy selector={`.diodes .st0:nth-child(n+${voltsWhole + 1})`} class="st0" />
+			<SvgProxy selector={`.diodes .st0:nth-child(${volts - voltsWhole ? voltsWhole + 1 : 0})`} class="st0 partial" />
+		</SvgLoader>
+	);
 };
 
 Bargraph.propTypes = {
-  power: PropTypes.number
+	volts: PropTypes.number
 };
 
 export default Bargraph;
