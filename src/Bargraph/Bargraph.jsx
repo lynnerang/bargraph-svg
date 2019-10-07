1cfbb2f69fdd0f0bb73e5950c348cfc762e4b2c9
Lynne Rang <lynne.rang@gmail.com>
2019-10-07 08:29:12 -0600

    Add group to svg code and interpolate power.  It does not go back down though.

--- src/Bargraph/Bargraph.jsx
+++ src/Bargraph/Bargraph.jsx
@@ -11,13 +11,19 @@ const Bargraph = (props) => {
     return power % 1 === 0 ? result : `${result} next-partial`;
   }
 
+  console.log(Math.floor(props.power))
+
   return (
     <SvgLoader className={getClass()} path={Image}>
-      <SvgProxy selector="rect:nth-of-type(4)" class="st0 diode-5" />
+      <SvgProxy
+        selector={`.diodes .st0:nth-child(-n+${Math.floor(props.power)})`}
+        class="st0 active"
+      />
+      {/* <SvgProxy selector="rect:nth-of-type(4)" class="st0 diode-5" />
       <SvgProxy selector="rect:nth-of-type(6)" class="st0 diode-4" />
       <SvgProxy selector="rect:nth-of-type(8)" class="st0 diode-3" />
       <SvgProxy selector="rect:nth-of-type(10)" class="st0 diode-2" />
-      <SvgProxy selector="rect:nth-of-type(12)" class="st0 diode-1" />
+      <SvgProxy selector="rect:nth-of-type(12)" class="st0 diode-1" /> */}
     </SvgLoader>
   );
 };
